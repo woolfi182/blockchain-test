fb01e78cc160c93bc5910a9aa4682be4f213b433
Maksym Mosiura <maksymmosiura@gmail.com>
2023-04-03 10:54:48 +0100

    server: remove redundant provider

--- nodejs-server/src/providers/index.js
+++ nodejs-server/src/providers/index.js
@@ -1,7 +1,5 @@
 const DbProvider = require("./db.provider");
-const EthProvider = require("./eth.provider");
 
 module.exports = {
   DbProvider,
-  EthProvider,
 };
--- nodejs-server/src/services/index.js
+++ nodejs-server/src/services/index.js
@@ -1,7 +1,5 @@
 const TrxService = require("./trx.service");
-const EthService = require("./eth.service");
 
 module.exports = {
   TrxService,
-  EthService,
 };
